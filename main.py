be05b15930d8cd2b036bfb1c7df5b0bc2558f915
jordy <jordyaraujo@outlook.com>
2022-09-09 17:24:20 -0300

    Criando configurações de banco

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_mapping(
+        DATABASE=os.path.join(app.instance_path, f'{__name__}.sqlite'),
+    )
 
     try:
         os.makedirs(app.instance_path)
